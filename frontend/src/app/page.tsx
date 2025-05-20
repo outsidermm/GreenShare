7fa5e2a93a642caab3d996cf3e592b2eda5d356c
outsidermm <71582657+outsidermm@users.noreply.github.com>
2025-05-21 01:02:50 +1000

    added placeholder homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
-import LoginForm from "../components/LoginForm";
-
 export default function Home() {
   return (
-    <div className="bg-white w-screen h-screen flex items-center">
-        <LoginForm />
+    <div className="bg-white w-screen h-screen flex items-center flex-col py-96">
+      <div className="m-auto">
+        <h1 className="text-4xl text-center text-slate-800 font-bold flex-col">GreenShare</h1>
+      </div>
+      <div className="m-auto">
+        <h2 className="text-2xl text-center text-slate-800 font-bold">Give more, Waste less, Share better</h2>
+      </div>
     </div>
   );
 }
