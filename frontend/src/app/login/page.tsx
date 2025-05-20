463b15e6e3d8bb8fc41b93faceaa31bcabe6fa8d
outsidermm <71582657+outsidermm@users.noreply.github.com>
2025-05-21 01:54:33 +1000

    add login page on its own route

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "../../components/LoginForm";
+
+export default function Home() {
+  return (
+    <div className="bg-white w-screen h-screen flex items-center">
+        <LoginForm />
+    </div>
+  );
+}
