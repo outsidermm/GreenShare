4ce4429589125cb016688d7e725c004dee987974
outsidermm <71582657+outsidermm@users.noreply.github.com>
2025-05-31 16:25:41 +1000

    add product fetch and viewing to generalised category pages

--- frontend/src/app/category/[category]/page.tsx
+++ frontend/src/app/category/[category]/page.tsx
@@ -4,11 +4,29 @@ import { useRouter, usePathname } from "next/navigation";
 import logoutUser from "@/services/logoutUser";
 import NavBar from "@/components/NavBar";
 import HeaderBar from "@/components/HeaderBar";
+import { useEffect, useState } from "react";
+import getItem from "@/services/getItem";
+
+interface Item {
+    id: string;
+    user_id: string;
+    title: string;
+    description: string;
+    condition: string;
+    status: string;
+    location: string;
+    category: string;
+    type: string;
+    updated_at: string;
+    images: string[];
+}
 
 export default function Home() {
   const { isAuthenticated, refreshAuth } = useAuth();
   const router = useRouter();
   const pathname = usePathname();
+  const [items, setItems] = useState<Array<Item>>([]);
+  
 
   const categories = [
     { label: "Essentials", path: "/category/essentials" },
@@ -17,6 +35,21 @@ export default function Home() {
     { label: "Style & Expression", path: "/category/style-expression" },
     { label: "Leisure & Learning", path: "/category/leisure-learning" },
   ];
+
+  useEffect (() => {
+    const fetchItems = async () => {
+      try {
+        const category_filter = pathname.replace("/category/", "")
+        console.log(category_filter)
+        const response = await getItem({category: category_filter});
+        setItems(response);
+      } catch (error) {
+        console.error("Error fetching items:", error);
+      }
+    };
+
+    fetchItems();
+},[pathname])
 
   const handleLogin = async () => {
     router.push("/login");
@@ -48,19 +81,22 @@ export default function Home() {
 
       <div className="ml-60 p-6">
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
-          {[
-            { name: "Nintendo Switch", condition: "Brand new" },
-            { name: "Sony A7s III", condition: "Slightly used" },
-          ].map((item, i) => (
-            <div key={i} className="bg-white rounded shadow p-4">
-              <div className="bg-slate-500 h-32 mb-3 rounded" />
-              <h4 className="text-slate-800 font-bold">{item.name}</h4>
-              <p className="text-blue-600">{item.condition}</p>
-              <button className="mt-2 text-sm bg-blue-500 text-white px-3 py-1 rounded hover:bg-green-600">
-                Make a Offer
-              </button>
+          {items.length > 0 ? (
+            items.map((item) => (
+              <div key={item.id} className="bg-white rounded shadow p-4">
+                <div className="bg-slate-500 h-32 mb-3 rounded" />
+                <h4 className="text-slate-800 font-bold">{item.title}</h4>
+                <p className="text-blue-600">{item.condition}</p>
+                <button className="mt-2 text-sm bg-blue-500 text-white px-3 py-1 rounded hover:bg-green-600">
+                  Make an Offer
+                </button>
+              </div>
+            ))
+          ) : (
+            <div className="col-span-4 text-center text-slate-600">
+              <p>No items available at the moment.</p>
             </div>
-          ))}
+          )}
         </div>
       </div>
     </div>
