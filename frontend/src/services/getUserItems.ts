67e742d407f2df9b2c23aaa7b1e1fce3e10222d5
outsidermm <71582657+outsidermm@users.noreply.github.com>
2025-06-12 00:18:46 +1000

    added frontend connection to the new authorised route to view user items

---
+++ frontend/src/services/getUserItems.ts
@@ -0,0 +1,33 @@
+import { Item } from "@/types/item";
+
+const API_BASE = process.env.NEXT_PUBLIC_API_URL;
+
+export default async function getItems(): Promise<Item[]> {
+  try {
+		const csrf_token = localStorage.getItem("csrfToken");
+    if (!csrf_token) {
+      throw new Error("No CSRF token found");
+    }
+
+    const response = await fetch(`${API_BASE}/item/userview`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+				"X-CSRF-Token": csrf_token,
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error || "Unknown error occurred");
+    }
+
+    const result: Item[] = await response.json();
+    console.log("Response from server:", result);
+    return result;
+  } catch (error) {
+    console.error("Error fetching item:", error);
+    throw error; // Re-throw the error so it can be handled by the caller
+  }
+}
