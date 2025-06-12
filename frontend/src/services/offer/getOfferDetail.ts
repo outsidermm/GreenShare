f39fd0f030bcb0e7050a2b85ea5f4833f733f5ca
outsidermm <71582657+outsidermm@users.noreply.github.com>
2025-06-13 00:09:19 +1000

    added the frontend connection the route to completeoffer

---
+++ frontend/src/services/offer/getOfferDetail.ts
@@ -0,0 +1,30 @@
+import { Offer } from "@/types/offer";
+
+const API_BASE = process.env.NEXT_PUBLIC_API_URL;
+
+export default async function confirmOfferComplete(offerId : number): Promise<Offer> {
+  try {
+    const params: URLSearchParams = new URLSearchParams();
+    params.append("offerId", offerId.toString());
+
+    const response = await fetch(`${API_BASE}/offer/details?${params.toString()}`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error || "Unknown error occurred");
+    }
+
+    const result: Offer = await response.json();
+    console.log("Response from server:", result);
+    return result;
+  } catch (error) {
+    console.error("Error fetching item:", error);
+    throw error; // Re-throw the error so it can be handled by the caller
+  }
+}
