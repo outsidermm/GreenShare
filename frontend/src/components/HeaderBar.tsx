5a635e8ffa276b506aaa9e2c20676dbe3684ef8d
outsidermm <71582657+outsidermm@users.noreply.github.com>
2025-06-16 22:00:02 +1000

    add clearing search term and submitting search term button which are linked up to parent component filters

--- frontend/src/components/HeaderBar.tsx
+++ frontend/src/components/HeaderBar.tsx
@@ -2,14 +2,16 @@
 import searchItem from "@/services/api/searchItem";
 import { toTitleCase } from "@/utils/titleCase";
 import { useState, useEffect } from "react";
+import { ImCross } from "react-icons/im";
 
 interface HeaderBarProps {
   isAuthenticated: boolean;
   handleLogin: () => void;
+  handleTitleFilter: (title: string) => void;
 }
 
 export default function HeaderBar(HeaderBarProps: HeaderBarProps) {
-  const { isAuthenticated, handleLogin } = HeaderBarProps;
+  const { isAuthenticated, handleLogin , handleTitleFilter} = HeaderBarProps;
   const [searchTerm, setSearchTerm] = useState("");
   const [options, setOptions] = useState<string[]>([]);
 
@@ -36,17 +38,45 @@ export default function HeaderBar(HeaderBarProps: HeaderBarProps) {
 
     return () => clearTimeout(timer);
   }, [searchTerm]);
-  
+
   return (
     <>
       <div className="text-lg font-bold text-green-600">GreenShare</div>
-      <div className="flex items-center gap-4 flex-grow px-4 relative">
+      <div className="flex items-center gap-4 flex-grow px-4 pr-10 relative">
         <input
           type="text"
           placeholder="Search for items"
           className="flex-grow px-3 py-2 rounded-xl border border-white focus:border-green-500 outline-none placeholder-white bg-inherit text-sm"
-          onChange={(e)=> setSearchTerm(e.target.value)}
+          onChange={(e)=> {setSearchTerm(e.target.value)
+            if (e.target.value.length === 0) {
+              handleTitleFilter(""); 
+            }
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleTitleFilter(searchTerm);
+            }
+          }}
+          value={searchTerm}
         />
+        {searchTerm && (
+            <button
+              onClick={() => {
+                setSearchTerm("");
+                handleTitleFilter("");
+              }}
+              className="absolute right-28 px-3 text-xs"
+            >
+              <ImCross className="text-white" />
+            </button>
+        )}
+        <button
+          onClick={() => handleTitleFilter(searchTerm)}
+          className="absolute right-10 text-white text-sm bg-green-600 hover:bg-green-700 px-3 py-2 rounded-r-xl"
+        >
+          Enter
+        </button>
+
         {options && options.length > 0 && (
           <div className="absolute top-10 flex-grow left-4 bg-white border shadow-xl rounded-xl" style={{ width: "calc(100% - 2rem)" }}>
             {options.map((option, index) => (
