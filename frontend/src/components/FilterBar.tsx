d8781ac0a6560f68abacde9b09de0fb1b6c0a9dd
outsidermm <71582657+outsidermm@users.noreply.github.com>
2025-07-01 15:09:16 +1000

    added optional width prop to set constant width, ensure no width change when clicking upon dropdown

--- frontend/src/components/FilterBar.tsx
+++ frontend/src/components/FilterBar.tsx
@@ -32,6 +32,7 @@ export default function FilterBar(FilterBarProps: FilterBarProps) {
         selectedOption={conditionFilter}
         setSelectedOption={(option) => handleConditionFilter(option)}
         isClearable={true}
+        width="16rem"
       />
       <DropDown
         label_text="Filtered Type:"
@@ -40,6 +41,7 @@ export default function FilterBar(FilterBarProps: FilterBarProps) {
         options={typeOptions}
         setSelectedOption={(option) => handleTypeFilter(option)}
         isClearable={true}
+        width="16rem"
       />
     </section>
   );
